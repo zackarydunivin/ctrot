6385c20cb01579b9d7865de06de5baa90a216fbe
Vincent Wong <vmwong1@gmail.com>
2019-02-18 23:13:07 -0500

    Updates to run networks for larger bins

--- network_analysis/building_data/build_networks_ez.py
+++ network_analysis/building_data/build_networks_ez.py
@@ -1,24 +1,39 @@
 # -*- coding: utf-8 -*-
 
-from tqdm import tqdm
 import numpy as np
 import networkx as nx
 import sqlite3 as s3
 from datetime import datetime, timedelta
 import pickle
+import sys
+from tqdm import tqdm
+import os
 
-conn = s3.connect('CTROT')
+conn = s3.connect('CTROT_mid')
 c = conn.cursor()
 
 start_date = datetime.strptime("2009-10-21 00:00:00","%Y-%m-%d 00:00:00")
 end_date = datetime.strptime("2017-10-24 00:00:00","%Y-%m-%d 00:00:00")
 total_days = 2925
+binsize = int(sys.argv[1])
 
-folder_n = "rt_networks/"
+folder_n = "/N/dc2/scratch/vmwong/rt_networks/"
 
-for current_date in tqdm([start_date + timedelta(x) for x in range(total_days-7)]):
+l1 = set([]); l2 = set([]);
+for x in os.listdir(folder_n):
+    thing = x.split("_")[0]
+    if thing not in l1:
+        l1.add(thing)
+    else:
+        l2.add(thing)
+done_dates = l1 & l2
+
+for current_date in tqdm([start_date + timedelta(x) for x in range(total_days-binsize)]):
+    print(current_date)
+    if current_date.strftime("%Y-%m-%d") in done_dates:
+        continue
     g = nx.Graph()
-    for c_date in [current_date + timedelta(i) for i in range(7)]:
+    for c_date in [current_date + timedelta(i) for i in range(binsize)]:
         c.execute("SELECT * FROM retweets WHERE created_at='"+c_date.strftime("%Y-%m-%d 00:00:00")+"'")
         for user in c.fetchall():
             g.add_edge(user[1],user[2])
